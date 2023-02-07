0d5c4ebc0acba51ce1d4e0877b7906c9f3bb3402
Saharat Nuamchit <saharat.nuamchit@gmail.com>
2023-02-07 15:13:05 +0700

    fix: styling

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -33,18 +33,24 @@ export function LinkButton({
   href,
   children,
   className,
+  onMouseOver,
+  onMouseLeave,
 }: {
   type: "primary" | "secondary"
   children: ReactNode
   href: string
   className?: string
+  onMouseOver?: ()=> void
+  onMouseLeave?: ()=> void
 }) {
   return (
     <Link href={href}>
       <a
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
         className={clsx(
-          "py-2 px-14 rounded-2xl transition-colors duration-200",
-          type === "primary" && "bg-vlvu-pink-400 text-vlvu-pink-500 hover:bg-vlvu-pink-500 hover:text-white",
+          "py-2 px-14 rounded-2xl transition-colors duration-200 w-[12rem] text-center",
+          type === "primary" && "bg-vlvu-pink-400 text-vlvu-pink-500 hover:bg-vlvu-pink-500 hover:text-white hover:{children=value}",
           type === "secondary" && "bg-vlvu-pink-500 text-white hover:bg-vlvu-pink-600",
           className
         )}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { LinkButton } from "@components/common/Button"
 import { easeInOut, motion } from "framer-motion"
 import { DescribeRoute } from "@components/Meta/DescribeRoute"
+import React from 'react';
+import { useState } from "react";
 
 function WelcomeText() {
+  const initText = "Ready ?";
+  const [text,setText] = useState(initText);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -12,12 +17,12 @@ function WelcomeText() {
       className="flex flex-col items-center justify-center h-screen gap-6"
     >
       <div>
-        <h1 className="font-semibold text-lg text-center">Welcome!</h1>
-        <p className="text-center">มาตามหาดอกไม้สำหรับคุณกัน</p>
+        <h1 className="text-lg text-center text-[32px] h-[39px]">Welcome!</h1>
+        <p className="text-center mt-[7px]">มาตามหาดอกไม้สำหรับคุณกัน</p>
       </div>
 
-      <LinkButton href="/register" type="primary">
-        Ready ?
+      <LinkButton href="/register" type="primary" onMouseOver={()=>{setText("Let start !")}} onMouseLeave={() => {setText(initText)}}>
+        {text}
       </LinkButton>
     </motion.div>
   )
@@ -26,7 +31,7 @@ function WelcomeText() {
 export default function Home() {
   return (
     <DescribeRoute title="Vid Love Vid U" description="Vid Love Vid U">
-      <div className="bg-vlvu-pink-100 font-display min-h-screen w-full">
+      <div className="bg-vlvu-pink-100 font-display min-h-screen w-full font-semibold ">
         <main className="text-vlvu-pink-500 mx-auto max-w-lg">
           <WelcomeText />
         </main>
