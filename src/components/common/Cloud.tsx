3b49548bf06d82adce0dbf5930c0153433cdfc26
Saharat Nuamchit <saharat.nuamchit@gmail.com>
2023-02-10 19:46:44 +0700

    feat: cloud

---
+++ src/components/common/Cloud.tsx
@@ -0,0 +1,25 @@
+import { motion, Variants } from "framer-motion"
+import Image from "next/image"
+
+export const Cloud = ({
+    variants,
+}:{
+    variants:Variants
+}) => {
+    return(
+        <motion.div
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="end"
+            transition={{duration: 1}}
+        >
+        <div className="items-center justify-items-center">
+            <Image src="/assets/cloud.svg" width="160px" height="64px" />
+        </div>
+        </motion.div>
+    )
+}
+
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { DescribeRoute } from "@/components/Meta/DescribeRoute"
 import React from "react"
 import { useState } from "react"
 import Image from "next/image"
+import { Cloud } from "@/components/common/Cloud"
 
 function WelcomeText() {
   const initText = "Ready ?"
@@ -41,24 +42,75 @@ function WelcomeText() {
 function FlyInBackground() {
   return(
     <motion.div
-      initial={{ opacity: 1 , y: 100 }}
-      animate={{ opacity: 1 , y: 0 }}
+      initial={{ opacity: 0 , y: "-100vh" }}
+      animate={{ opacity: 1 , y: "10vh" ,x: "0"}}
       exit={{ opacity: 0 }}
-      transition={{duration: 3}}
+      transition={{duration: 1}}
     >
-      <div>
+      <div className="items-center justify-items-center">
         <Image src="/assets/purple-bg.svg" width="318px" height="245.5px" />
       </div>
     </motion.div>
   )
 }
 
+const variants1 = {
+  hidden: {
+    opacity: 0 , 
+    y: "-100vh"
+  },
+  visible: {
+    opacity: 1 , 
+    y: "0vh" ,
+    x: "0"
+  },
+  end:{
+
+  }
+}
+
+const variants2 = {
+  hidden: {
+    opacity: 0 , 
+    y: "-100vh"
+  },
+  visible: {
+    opacity: 1 , 
+    y: "20vh" ,
+    x: "0"
+  },
+  end:{
+
+  }
+}
+
+const variants3 = {
+  hidden: {
+    opacity: 0 , 
+    y: "-100vh"
+  },
+  visible: {
+    opacity: 1 , 
+    y: "40vh" ,
+    x: "0"
+  },
+  end:{
+
+  }
+}
+
+
 export default function Home() {
   return (
     <DescribeRoute title="Vid Love Vid U" description="Vid Love Vid U">
       <div className="bg-vlvu-pink-100 font-display min-h-screen w-full font-semibold ">
         <main className="text-vlvu-pink-500 mx-auto max-w-lg">
-          <FlyInBackground />
+          <div>
+            <FlyInBackground />
+          </div>
+          <Cloud variants={variants1}></Cloud>
+          <Cloud variants={variants2}></Cloud>
+          <Cloud variants={variants3}></Cloud>
           <WelcomeText />
         </main>
       </div>
