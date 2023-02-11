afed5d31ab11e2f4aea465e22cffd5fa01c2bd83
betich <betich365@gmail.com>
2023-02-11 18:00:57 +0700

    feat: tweak google button styles

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -21,23 +21,15 @@ export default function Register() {
           </div>
           <div className="flex flex-col gap-3">
             <Button
-              type="primary"
+              type="white"
               onClick={() => {
                 auth?.signinWithGoogle("/registerform")
               }}
-              className="bg-white flex gap-6 px-8 py-3 items-center content-start"
+              className="flex gap-3 px-8 py-3 items-center shadow-md content-start"
             >
               <GoogleIcon height="32" width="32" className="" />
-              <span className="grow text-start text-gray-500 font-semibold">Sign in with Google</span>
+              <span className="grow text-start text-lg font-semibold">Sign in with Google</span>
             </Button>
-            {/* <LinkButton
-              type="primary"
-              href="/emailregister"
-              className="bg-white flex gap-6 px-8 items-center content-start"
-            >
-              <Image src="/assets/email.svg" alt="email icon" height="32px" width="32px" />
-              <span className="grow text-start text-gray-500 font-semibold">Sign in with Email</span>
-            </LinkButton> */}
           </div>
         </div>
       </main>
