c4e14fed9f9b2a530ec2a29d9322dad7d850a883
betich <betich365@gmail.com>
2023-02-11 17:46:44 +0700

    feat: fix og banner link and edit description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,11 @@ import { AuthProvider } from "@/lib/auth"
 import { DescribeRoute } from "@/components/Meta/DescribeRoute"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <DescribeRoute title="Vid Love Vid U" description="14 Feb 2023" imgURL="/assets/banner.png">
+    <DescribeRoute
+      title="Vid Love Vid U"
+      description="วันที่ 14 กุมภาพันธ์ 2566 เวลา 16.30 เป็นต้นไป ณ ลานจักร-ลานเกียร์"
+      imgURL="/assets/meta/banner.png"
+    >
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
