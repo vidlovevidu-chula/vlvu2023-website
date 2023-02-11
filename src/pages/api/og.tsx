0d29b2e234daf4b7c41983a2ec95010e217da3d4
betich <betich365@gmail.com>
2023-02-11 19:43:41 +0700

    fix: add CORS headers

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -16,81 +16,86 @@ const BGImgSrc = fetch(new URL("../../images/ticket/bg.png", import.meta.url))
 const SIZE = 180
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const url = new URL(req.url ?? "")
-  const uid = url.searchParams.get("uid") ?? ""
-  const name = url.searchParams.get("name") ?? ""
-  const type = ["daisy", "forget_me_not", "rose", "sunflower", "tulip"].includes(url.searchParams.get("type") ?? "")
-    ? (url.searchParams.get("type") as string)
-    : "daisy"
+  try {
+    const url = new URL(req.url ?? "")
+    const uid = url.searchParams.get("uid") ?? ""
+    const name = url.searchParams.get("name") ?? ""
+    const type = ["daisy", "forget_me_not", "rose", "sunflower", "tulip"].includes(url.searchParams.get("type") ?? "")
+      ? (url.searchParams.get("type") as string)
+      : "daisy"
 
-  const RobotoSlabData = await RobotoSlab
+    const RobotoSlabData = await RobotoSlab
 
-  const base64 = qrcode(uid, {
-    size: SIZE,
-  })
+    const base64 = qrcode(uid, {
+      size: SIZE,
+    })
 
-  // render flower image based on
-
-  // Flowers
-
-  // const flowerImgSrc = fetch(`${req.url}/assets/flower/${type}.png`)
-  //   .then((res) => res.blob())
-  //   .then((blob) => URL.createObjectURL(blob))
-
-  return new ImageResponse(
-    (
-      <div
-        style={{ fontFamily: "Roboto Slab" }}
-        tw="text-6xl bg-white font-bold bg-[#FFF2F4] w-full h-full flex flex-col items-center justify-center text-center relative"
-      >
-        <div tw="absolute flex top-0 left-0">
-          <img alt="bg" height={1300} width={620} src={await BGImgSrc} />
-        </div>
+    return new ImageResponse(
+      (
         <div
-          style={{
-            zIndex: "50",
-            top: name.length <= 13 ? 130 : 145,
-            left: "50%",
-            transform: "translateX(-50%)",
-          }}
-          tw="flex absolute"
+          style={{ fontFamily: "Roboto Slab" }}
+          tw="text-6xl bg-white font-bold bg-[#FFF2F4] w-full h-full flex flex-col items-center justify-center text-center relative"
         >
-          <p
+          <div tw="absolute flex top-0 left-0">
+            <img alt="bg" height={1300} width={620} src={await BGImgSrc} />
+          </div>
+          <div
             style={{
-              fontSize: name.length <= 13 ? 64 : 36,
+              zIndex: "50",
+              top: name.length <= 13 ? 130 : 145,
+              left: "50%",
+              transform: "translateX(-50%)",
             }}
-            tw="text-[#C697C5]"
+            tw="flex absolute"
           >
-            {name.length <= 18 ? name : name.slice(0, 18) + "..."}
-          </p>
-        </div>
-        <div style={{ zIndex: "50", bottom: 90, left: 75 }} tw="flex absolute">
-          <img alt="QR" height={SIZE} src={base64} width={SIZE} />
-        </div>
+            <p
+              style={{
+                fontSize: name.length <= 13 ? 64 : 36,
+              }}
+              tw="text-[#C697C5]"
+            >
+              {name.length <= 18 ? name : name.slice(0, 18) + "..."}
+            </p>
+          </div>
+          <div style={{ zIndex: "50", bottom: 90, left: 75 }} tw="flex absolute">
+            <img alt="QR" height={SIZE} src={base64} width={SIZE} />
+          </div>
 
-        <div style={{ zIndex: "50", bottom: 130, right: 90 }} tw="flex absolute">
-          <img alt="flower" height={150} src={`${url.origin}/assets/flower/${type}.png`} width={150} />
-          {/* {type === "daisy" && <img alt="daisy" height={150} src={await daisyImgSrc} width={150} />}
+          <div style={{ zIndex: "50", bottom: 130, right: 90 }} tw="flex absolute">
+            <img alt="flower" height={150} src={`${url.origin}/assets/flower/${type}.png`} width={150} />
+            {/* {type === "daisy" && <img alt="daisy" height={150} src={await daisyImgSrc} width={150} />}
           {type === "forget_me_not" && (
             <img alt="forget_me_not" height={150} src={await forgetMeNotImgSrc} width={150} />
           )}
           {type === "rose" && <img alt="rose" height={150} src={await roseImgSrc} width={150} />}
           {type === "sunflower" && <img alt="sunflower" height={150} src={await sunflowerImgSrc} width={150} />}
           {type === "tulip" && <img alt="tulip" height={150} src={await tulipImgSrc} width={150} />} */}
+          </div>
         </div>
-      </div>
-    ),
-    {
-      width: 620,
-      height: 1300,
-      fonts: [
-        {
-          name: "Roboto Slab",
-          data: RobotoSlabData,
-          weight: 600,
-          style: "normal",
+      ),
+      {
+        width: 620,
+        height: 1300,
+        fonts: [
+          {
+            name: "Roboto Slab",
+            data: RobotoSlabData,
+            weight: 600,
+            style: "normal",
+          },
+        ],
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers":
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
         },
-      ],
-    }
-  )
+      }
+    )
+  } catch (e: any) {
+    console.error(`${e.message}`)
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
 }
